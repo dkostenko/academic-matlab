d13bfc790f823f848ea51921e78969372cda7e5b
dkostenko <kostenko.personal@gmail.com>
2012-11-09 02:07:01 +0400

    kdz

--- lukas_kanade_test.m
+++ lukas_kanade_test.m
@@ -1,18 +1,26 @@
 % Im = imread('cameraman.tif');
 % Im = imread('pout.tif');
-Im = imread('trees.tif');
+% Im = imread('trees.tif');
+% Im = imread('moon.tif');
 
-DGaussx = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
-DGaussy = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+Im1 = imread('/Users/macbook/Downloads/OpticalFlow/table1.jpg');
+Im2 = imread('/Users/macbook/Downloads/OpticalFlow/table2.jpg');
 
-
-Ix = imfilter(Im,DGaussx,'conv');
-Iy = imfilter(Im,DGaussy,'conv');
-
-newIm = Ix + Iy;
+proizvodnayaX = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
+proizvodnayaY = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
 
 
-subplot(2,2,1), subimage(uint8(Im));
-subplot(2,2,2), subimage(Ix);
-subplot(2,2,3), subimage(Iy);
-subplot(2,2,4), subimage(newIm);
+Ix1 = imfilter(Im1,proizvodnayaX,'conv');
+Iy1 = imfilter(Im1,proizvodnayaY,'conv');
+newIm1 = Ix1 + Iy1;
+
+
+Ix2 = imfilter(Im2,proizvodnayaX,'conv');
+Iy2 = imfilter(Im2,proizvodnayaY,'conv');
+newIm2 = Ix2 + Iy2;
+
+newIm3 = Im2 - Im1;
+
+subplot(2,2,1), subimage(newIm1);
+subplot(2,2,2), subimage(newIm2);
+subplot(2,2,3), subimage(newIm3);
